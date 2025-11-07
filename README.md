75937c18fedfe56b6de6b505320570c0e792d09b
rosenrose <crazytempler@gmail.com>
2025-11-07 17:38:28 +0900

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+```
+uv python pin 3.11
+
+uv init
+
+uv add -r requirements.txt
+```
---
+++ main.py
@@ -0,0 +1,10 @@
+import tiktoken
+import langchain
+
+
+def main():
+    print(tiktoken, langchain)
+
+
+if __name__ == "__main__":
+    main()
