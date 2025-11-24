21d99b6aad717e91cd7d336e1af10cb16ccb9db3
rosenrose <crazytempler@gmail.com>
2025-11-24 17:07:03 +0900

    7.1 write, magic

--- Home.py
+++ Home.py
@@ -5,3 +5,20 @@ st.subheader("Welcome to Streamlit")
 st.markdown("""
     #### I love it.
 """)
+
+
+class A:
+    def __init__(self):
+        pass
+
+
+1
+2
+3
+st.write("abc")
+st.write([1, 2, 3])
+st.write((1, 2, 3))
+st.write({"a": None, "b": set([1, 2, 3]), "c": st.title})
+st.write(A)
+st.write(st)
+st.write("a", None, True, False)
