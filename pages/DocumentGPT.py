264403e648690ff91f8b87a653d76ff6a6603977
rosenrose <crazytempler@gmail.com>
2025-11-25 17:44:19 +0900

    7.6 Uploading Documents

--- pages/DocumentGPT.py
+++ pages/DocumentGPT.py
@@ -1,27 +1,46 @@
+import io
 import streamlit as st
-import time
+from langchain_classic.embeddings import CacheBackedEmbeddings
+from langchain_classic.storage import LocalFileStore
+from langchain_community.document_loaders import UnstructuredFileIOLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
 
 st.title("Document GPT")
-st.session_state["messages"] = st.session_state.get("messages", [])
+st.set_page_config(page_title="Document GPT", page_icon="📄")
 
 
-def send_message(message, role, is_save=True):
-    with st.chat_message(role):
-        st.write(message)
+def embed_fiile(file):
+    file_io = io.BytesIO(file.read())
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings, cache_dir, key_encoder="sha256"
+    )
+    loader = UnstructuredFileIOLoader(file_io)
+    docs = loader.load_and_split(text_splitter=splitter)
+    vectorstore = Chroma.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
 
-    if is_save:
-        st.session_state["messages"].append({"message": message, "role": role})
+    return retriever
 
 
-for message in st.session_state["messages"]:
-    send_message(message["message"], message["role"], is_save=False)
+chat = ChatOpenAI(
+    temperature=0.1,
+    # streaming=True,
+    # callbacks=[StreamingStdOutCallbackHandler()]
+)
+splitter = CharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=600, chunk_overlap=100, separator="\n"
+)
+embeddings = OpenAIEmbeddings()
 
-message = st.chat_input("Send msg")
+st.markdown("""
+Use this chatbot to ask question to AI about your documents.
+""")
 
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
+file = st.file_uploader(".txt, .pdx, .docx", type=["txt", "pdf", "docx"])
 
-    with st.sidebar:
-        st.write(st.session_state)
+if file:
+    retriever = embed_fiile(file)
+    st.write(retriever.invoke("ministry of truth"))
